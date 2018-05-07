78ff0b25262b80191f7494c3440cd666af5ed612
Cp, Sajith - Sajith <sacp@lowes.com>
2018-05-07 19:56:02 +0530

    Added a new method for addition

---
+++ ConsoleApp1/ConsoleApp1/Feature1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Feature1
+    {
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
+    }
+}
